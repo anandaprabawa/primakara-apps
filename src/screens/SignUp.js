d72ede7354c2d63d2d3e5e32df39f7ac35ae96d9
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2019-10-25 23:41:55 +0800

    add(functionality): sign up user

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,51 +1,104 @@
 import React from 'react';
 import {View, StyleSheet, ScrollView, StatusBar} from 'react-native';
-import {TextInput, Text, withTheme, Button} from 'react-native-paper';
+import {TextInput, Text, withTheme, Button, Snackbar} from 'react-native-paper';
+import firebase from 'react-native-firebase';
 
-const SignUp = ({theme}) => {
+const SignUp = ({theme, navigation}) => {
+  /* State to temporary save data only on this screen (SignUp) when `handleChangeTextInput` invoked */
   const [field, setField] = React.useState({name: '', email: '', password: ''});
 
+  /* Loading state used while waiting response from firebase */
+  const [loading, setLoading] = React.useState(false);
+
+  /* Error state */
+  const [errorMessage, setErrorMessage] = React.useState(null);
+
+  /* Function to handle input change to save data in the state */
   const handleChangeTextInput = fieldName => text => {
     setField({...field, [fieldName]: text});
+    setErrorMessage(null);
+  };
+
+  /* Function to validate form inputs */
+  const validateForm = async () => {
+    if (field.email !== '' && field.name !== '' && field.password !== '') {
+      return Promise.resolve();
+    }
+    return Promise.reject('Fields cannot be empty');
+  };
+
+  /* Function to create user when sign up button pressed, then navigate to Home screen if success */
+  const handlePressSignUp = async () => {
+    setLoading(true);
+    try {
+      await validateForm().catch(error => {
+        throw new Error(error);
+      });
+      await firebase
+        .auth()
+        .createUserWithEmailAndPassword(field.email, field.password)
+        .catch(error => {
+          throw new Error(error);
+        });
+      /* You don't need to setLoading to false here as the screen will switch out (not pushed) */
+      navigation.navigate('Home');
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+
+  const handleDismissSnackBar = () => {
+    setErrorMessage(null);
   };
 
   return (
     <>
       <StatusBar backgroundColor={theme.colors.secondary} />
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.headerWrapper(theme.colors.secondary)}>
-          <Text style={styles.title}>Welcome,</Text>
-          <Text style={styles.subtitle}>Sign up to use our app!</Text>
-        </View>
-        <View style={styles.bodyContainer}>
-          <TextInput
-            label="Name"
-            value={field.name}
-            onChangeText={handleChangeTextInput('name')}
-            style={styles.textInput}
-          />
-          <TextInput
-            label="Email"
-            value={field.email}
-            onChangeText={handleChangeTextInput('email')}
-            style={styles.textInput}
-          />
-          <TextInput
-            label="Password"
-            value={field.password}
-            secureTextEntry
-            onChangeText={handleChangeTextInput('password')}
-            style={styles.textInput}
-          />
-          <View style={styles.buttonWrapper}>
-            <Button
-              mode="contained"
-              contentStyle={styles.contentButton}
-              style={styles.button}
-              color={theme.colors.secondary}>
-              Sign Up
-            </Button>
+      <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
+        <View style={styles.container}>
+          <View style={styles.headerWrapper(theme.colors.secondary)}>
+            <Text style={styles.title}>Welcome,</Text>
+            <Text style={styles.subtitle}>Sign up to use our app!</Text>
           </View>
+          <View style={styles.bodyContainer}>
+            <TextInput
+              label="Name"
+              value={field.name}
+              onChangeText={handleChangeTextInput('name')}
+              style={styles.textInput}
+            />
+            <TextInput
+              label="Email"
+              value={field.email}
+              onChangeText={handleChangeTextInput('email')}
+              style={styles.textInput}
+            />
+            <TextInput
+              label="Password"
+              value={field.password}
+              secureTextEntry
+              onChangeText={handleChangeTextInput('password')}
+              style={styles.textInput}
+            />
+            <View style={styles.buttonWrapper}>
+              <Button
+                mode="contained"
+                contentStyle={styles.contentButton}
+                style={styles.button}
+                color={theme.colors.secondary}
+                onPress={handlePressSignUp}
+                loading={loading}>
+                Sign Up
+              </Button>
+            </View>
+          </View>
+          <Snackbar
+            visible={!!errorMessage}
+            onDismiss={handleDismissSnackBar}
+            action={{label: 'Dismiss', onPress: handleDismissSnackBar}}>
+            {errorMessage}
+          </Snackbar>
         </View>
       </ScrollView>
     </>
@@ -53,8 +106,12 @@ const SignUp = ({theme}) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  scrollViewContentContainer: {
     flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    position: 'relative',
   },
   bodyContainer: {
     flex: 1,
