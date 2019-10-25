94dc2252a2762818eaaa72f961b6b95926a0d789
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2019-10-25 23:39:56 +0800

    add(functionality): temporary user sign out on Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
-import {Text, withTheme} from 'react-native-paper';
+import {Text, withTheme, Button} from 'react-native-paper';
+import firebase from 'react-native-firebase';
 
-const Home = () => {
+const Home = ({navigation}) => {
+  /* Function to logging out user */
+  const handlePressSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        navigation.navigate('Auth');
+      });
+  };
+
   return (
     <ScrollView>
       <Text>Hello World</Text>
+      <Button onPress={handlePressSignOut}>Sign Out</Button>
     </ScrollView>
   );
 };
