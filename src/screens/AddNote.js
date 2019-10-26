edb05f8a79c50b6f1770fd817402b78d39b8e3b7
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2019-10-26 23:36:26 +0800

    add: image picker from camera

--- src/screens/AddNote.js
+++ src/screens/AddNote.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {ScrollView, StyleSheet, View} from 'react-native';
-import {TextInput, Button, Snackbar} from 'react-native-paper';
+import {ScrollView, StyleSheet, View, Image, Dimensions} from 'react-native';
+import {TextInput, Button, Snackbar, IconButton} from 'react-native-paper';
+import ImagePicker from 'react-native-image-crop-picker';
 import firebase from 'react-native-firebase';
 
 const AddNoteScreen = ({navigation}) => {
   const [notes, setNotes] = React.useState({title: '', body: ''});
+  const [image, setImage] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState(null);
 
@@ -12,6 +14,29 @@ const AddNoteScreen = ({navigation}) => {
     setNotes({...notes, [field]: text});
     setErrorMessage(null);
   };
+
+  /* In this case, the image will be saved in Firestore. For real world, you should use Firebase Storage instead. */
+  const handlePressCamera = async () => {
+    try {
+      const imageResult = await ImagePicker.openCamera({});
+      const croppedImage = await ImagePicker.openCropper({
+        path: imageResult.path,
+        width: 512,
+        height: 512,
+        compressImageQuality: 0.7,
+        includeBase64: true,
+      });
+      const base64 = `data:${croppedImage.mime};base64,${croppedImage.data}`;
+      setImage(base64);
+      await ImagePicker.clean();
+    } catch (error) {
+      return null;
+    }
+  };
+
+  React.useEffect(() => {
+    navigation.setParams({handlePressCamera});
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const validateForm = () => {
     if (notes.title !== '' && notes.body !== '') {
@@ -27,7 +52,7 @@ const AddNoteScreen = ({navigation}) => {
       await firebase
         .firestore()
         .collection('notes')
-        .add(notes);
+        .add({...notes, image});
       navigation.goBack();
     } catch (error) {
       setLoading(false);
@@ -39,9 +64,28 @@ const AddNoteScreen = ({navigation}) => {
     setErrorMessage(null);
   };
 
+  const handleDismissImage = () => {
+    setImage(null);
+  };
+
   return (
     <ScrollView contentContainerStyle={styles.scrollviewContent}>
       <View style={styles.container}>
+        {image && (
+          <View>
+            <Image
+              style={styles.image}
+              source={{uri: image}}
+              resizeMode="cover"
+            />
+            <IconButton
+              icon="close"
+              color="#ffffff"
+              style={styles.closeImageIcon}
+              onPress={handleDismissImage}
+            />
+          </View>
+        )}
         <TextInput
           style={styles.field}
           label="Title"
@@ -75,10 +119,18 @@ const AddNoteScreen = ({navigation}) => {
   );
 };
 
-AddNoteScreen.navigationOptions = {
+AddNoteScreen.navigationOptions = ({navigation}) => ({
   title: 'Add Note',
-};
+  headerRight: (
+    <IconButton
+      icon="camera"
+      color="#ffffff"
+      onPress={navigation.getParam('handlePressCamera')}
+    />
+  ),
+});
 
+const screenWidth = Dimensions.get('screen').width;
 const styles = StyleSheet.create({
   scrollviewContent: {
     flexGrow: 1,
@@ -95,6 +147,17 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 'auto',
+  },
+  image: {
+    width: '100%',
+    height: screenWidth - 48,
+    marginBottom: 24,
+  },
+  closeImageIcon: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.3)',
   },
 });
 
