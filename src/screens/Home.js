dc8cee714366c1c40692a421ba9cdf0105ba7620
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2019-10-26 18:28:29 +0800

    improve: home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,29 +1,69 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
-import {Text, withTheme, Button} from 'react-native-paper';
+import {ScrollView, View, StyleSheet} from 'react-native';
+import {withTheme, IconButton, FAB, List} from 'react-native-paper';
 import firebase from 'react-native-firebase';
 
 const Home = ({navigation}) => {
   /* Function to logging out user */
-  const handlePressSignOut = () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        navigation.navigate('Auth');
-      });
+  const handlePressSignOut = async () => {
+    await firebase.auth().signOut();
+    navigation.navigate('Auth');
   };
 
+  /* Send `handlePressSignOut` function through navigation params to use in Home.navigationOptions below */
+  React.useEffect(() => {
+    navigation.setParams({handlePressSignOut});
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <ScrollView>
-      <Text>Hello World</Text>
-      <Button onPress={handlePressSignOut}>Sign Out</Button>
-    </ScrollView>
+    <View style={styles.container}>
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        onPress={() => console.log('Pressed')}
+      />
+      <ScrollView>
+        <List.Item
+          title="First title"
+          description="lorem ipsum dolor sit amet"
+          left={props => <List.Icon {...props} icon="note" />}
+        />
+        <List.Item
+          title="First title"
+          description="lorem ipsum dolor sit amet"
+          left={props => <List.Icon {...props} icon="note" />}
+        />
+      </ScrollView>
+    </View>
   );
 };
 
-Home.navigationOptions = {
-  title: 'Home',
-};
+Home.navigationOptions = ({navigation}) => ({
+  title: 'Primakara Notes',
+  headerRight: () => (
+    <IconButton
+      icon="logout"
+      color="#fff"
+      size={20}
+      onPress={navigation.getParam('handlePressSignOut')}
+    />
+  ),
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+  buttonListOptionWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 export default withTheme(Home);
